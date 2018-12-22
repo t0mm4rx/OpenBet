a22c65719fce919120723ab17a8d6c5536a56457
t0mm4rx <tom.marx@hotmail.fr>
2018-12-22 22:33:42 +0100

    Neural network slow

--- Libs/neural_network.py
+++ Libs/neural_network.py
@@ -9,12 +9,49 @@
         - guess(input (int[])): realise a prediction of the given input
 """
 
+import tensorflow as tf
+
 class NeuralNetwork:
 
-    def __init__(self, n_inputs, n_hidden, n_output):
+    def __init__(self, n_inputs, n_hiddens, n_outputs, learning_rate = 0.01):
         self.n_inputs = n_inputs
-        self.n_hidden = n_hidden
+        self.n_hiddens = n_hiddens
         self.n_outputs = n_outputs
+        self.learning_rate = learning_rate
+        self.session = None
+        self.optimizer = None
+        self.init_tf()
+
+    # Initializing tensorflow variables, weights and biases
+    def init_tf(self):
+        self.X = tf.placeholder("float", [None, self.n_inputs])
+        self.Y = tf.placeholder("float", [None, self.n_outputs])
+        self.weights = [None] * (len(self.n_hiddens) + 1)
+        self.bias = [None] * (len(self.n_hiddens) + 1)
+        for i in range(len(self.n_hiddens)):
+            if (i == 0):
+                self.weights[i] = tf.Variable(tf.random_normal([self.n_inputs, self.n_hiddens[i]]), name="weights_" + str(i))
+            else:
+                self.weights[i] = tf.Variable(tf.random_normal([self.n_hiddens[i - 1], self.n_hiddens[i]]), name="weights_" + str(i))
+        self.weights[len(self.n_hiddens)] = tf.Variable(tf.random_normal([self.n_hiddens[len(self.n_hiddens) - 1], self.n_outputs]), name="weights_" + str(i))
+        for i in range(len(self.bias) - 1):
+            self.bias[i] = tf.Variable(tf.zeros([self.n_hiddens[i]]), name="bias_" + str(i))
+        self.bias[len(self.bias) - 1] = tf.Variable(tf.zeros([self.n_outputs]), name="bias_" + str(len(self.bias) - 1))
+        self.session = tf.Session()
+        # Change here the optimizer later for experimentations
+        self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate)
+        self.session.run(tf.global_variables_initializer())
+        self.writer = tf.summary.FileWriter('Libs/LogsTF', self.session.graph)
+
+
+    def forward(self):
+        output = [None] * (len(self.n_hiddens) + 1)
+        #first = tf.sigmoid(tf.matmul([input], self.weights[0]) + self.bias[0])
+        output[0] = tf.sigmoid(tf.matmul(self.X,self.weights[0]) + self.bias[0])
+        for i in range(1, len(self.n_hiddens) + 1):
+            output[i] = tf.sigmoid(tf.matmul(output[i - 1], self.weights[i]) + self.bias[i])
+
+        return output[len(self.n_hiddens)]
 
     def load_weights(self, filename):
         return 0
@@ -23,7 +60,31 @@ class NeuralNetwork:
         return 0
 
     def train(self, input, output):
-        return 0
+        prediction = self.forward()
+        cost = tf.reduce_mean(-self.Y * tf.log(prediction) - (1 - self.Y) * tf.log(1 - prediction))
+        #print(self.session.run(cost))
+        self.session.run(self.optimizer.minimize(cost), feed_dict={self.X: [input], self.Y: [output]})
+        return self.session.run(cost, feed_dict={self.X: [input], self.Y: [output]})
 
     def guess(self, input):
-        return 0
+        return self.session.run(self.forward(), feed_dict={self.X: [input]})
+
+
+"""
+    Testing to resolve a xor to test the class
+"""
+
+nn = NeuralNetwork(2, [3, 3], 1, learning_rate=0.1)
+
+x_training = [ [0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0] ]
+y_training = [ [0.0], [1.0], [1.0], [0.0] ]
+
+average = 0
+
+for i in range(1000):
+    average += nn.train(x_training[i % 4], y_training[i % 4])
+    if (i % 4 == 3):
+        print(average / 4)
+        average = 0
+
+print(nn.guess([0, 0]))
